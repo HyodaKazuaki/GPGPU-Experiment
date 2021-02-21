de849ee7b4b78bbca32cce589ef3803975a2e3de
HyodaKazuaki <kazuaki@ex-t.jp>
2021-02-21 21:57:40 +0900

    add benchmark script

---
+++ ex2/gpu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# GPU automatic benchmark
+# Copyright © 2021 HyodaKazuaki
+#
+# Released under the MIT License
+# see https://opensource.org/licenses/MIT
+
+
+NUM=28
+AVERAGE=5
+FILENAME0="./logs/gpu0.csv"
+FILENAME1="./logs/gpu1.csv"
+
+echo "Start"
+
+for i in `seq 1 ${NUM}`
+do
+    echo "Phase $i processing..."
+    echo -n $i >> $FILENAME0
+    echo -n $i >> $FILENAME1
+    for j in `seq 1 ${AVERAGE}`
+    do
+        ./gpu.o $((2**$i)) 0 | tail -n 1 | awk '{print $2}' | awk -F [ '{print $1}' | echo -n ", $(cat)" >> $FILENAME0
+        ./gpu.o $((2**$i)) 1 | tail -n 1 | awk '{print $2}' | awk -F [ '{print $1}' | echo -n ", $(cat)" >> $FILENAME1
+    done
+    echo >> $FILENAME0
+    echo >> $FILENAME1
+done
+
+echo "Finished."
