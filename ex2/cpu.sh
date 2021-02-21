176cc7d6d67f0f09da996a7282ba8e3b1b10882e
HyodaKazuaki <kazuaki@ex-t.jp>
2021-02-21 22:00:02 +0900

    fix cpu benchmark script

--- ex2/cpu.sh
+++ ex2/cpu.sh
@@ -9,23 +9,19 @@
 
 NUM=28
 AVERAGE=5
-FILENAME0="./logs/cpu0.csv"
-FILENAME1="./logs/cpu1.csv"
+FILENAME="./logs/cpu.csv"
 
 echo "Start"
 
 for i in `seq 1 ${NUM}`
 do
     echo "Phase $i processing..."
-    echo -n $i >> $FILENAME0
-    echo -n $i >> $FILENAME1
+    echo -n $i >> $FILENAME
     for j in `seq 1 ${AVERAGE}`
     do
-        ./cpu.o $((2**$i)) 0 | tail -n 1 | awk '{print $2}' | awk -F [ '{print $1}' | echo -n ", $(cat)" >> $FILENAME0
-        ./cpu.o $((2**$i)) 1 | tail -n 1 | awk '{print $2}' | awk -F [ '{print $1}' | echo -n ", $(cat)" >> $FILENAME1
+        ./cpu.o $((2**$i)) | tail -n 1 | awk '{print $2}' | awk -F [ '{print $1}' | echo -n ", $(cat)" >> $FILENAME
     done
-    echo >> $FILENAME0
-    echo >> $FILENAME1
+    echo >> $FILENAME
 done
 
 echo "Finished."
